79eacfb7229d4474d5e4b4ae1ee44bb783420641
haruhi.m <halu0502@gmail.com>
2020-06-07 09:37:08 +0000

    Add all controllers and all views

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,10 @@
+class HousesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/houses_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class HousesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get houses_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get houses_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get houses_edit_url
+    assert_response :success
+  end
+
+end
