c5bef8a4e83f094fe2c4b361a44b09ebfa67f545
haruhi.m <halu0502@gmail.com>
2020-06-10 09:39:34 +0000

    Add views user_tags_index & user_tag_show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,16 @@
 class TagsController < ApplicationController
   def index
+  	@user = User.find(params[:user_id])
+  	@tags = []
+  	@memos = @user.memos.all
+  	@memos.each do |memo|
+  		@tags += memo.tags
+  	end
   end
 
   def show
+  	@user = User.find(params[:user_id])
+  	@tag = Memo.tag_counts.find(params[:id])
+  	@memos = Memo.tagged_with(@tag.name).where(user_id: @user.id)
   end
 end
