3bdf50cebbbb98992ad4fed3e2f9451347f7f1b4
haruhi.m <halu0502@gmail.com>
2020-06-12 10:21:14 +0000

    Add check method for stocks&likes

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -25,4 +25,15 @@ class Memo < ApplicationRecord
 		error_msg= "メモは必ずハウスかルームに入れてください"
 		errors.add(:house, error_msg) unless house_id || room_id
 	end
+
+	def stocked_by?(user)
+		stocks.where(user_id: user.id).exists?
+	end
+
+	# 指定のユーザにいいねされているか？
+	def liked_by?(user)
+		likes.where(user_id: user.id).exists?
+	end
+
+
 end
