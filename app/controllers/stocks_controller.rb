6180ab3130215818d3ec046b00a8b672de8b3189
haruhi.m <halu0502@gmail.com>
2020-06-12 10:41:19 +0000

    Update stocks_controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,27 @@
 class StocksController < ApplicationController
   def index
+  	@user = current_user
+  	@q = current_user.stock_memos.ransack(params[:q])
+  	@stocks = @q.result(distinct: true)
   end
+
+  def create
+  	# urlで(memo_id: @memo.id)を記述してparamsを渡す
+		current_user.stocks.create(memo_id: params[:memo_id])
+		flash[:success]="ストックに入れました"
+		redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+  	@stock = current_user.stocks.find_by(memo_id: params[:memo_id])
+  	if @stock.user_id == current_user.id
+  		@stock.destroy
+			flash[:alert]="ストックから削除しました"
+			redirect_back(fallback_location: root_path)
+		else
+			flash[:alert]="ストックから削除できませんでした"
+			redirect_back(fallback_location: root_path)
+		end
+  end
+
 end
