0114d8f14ba00a5c3b05673e129e2e2f7aa53e76
haruhi.m <halu0502@gmail.com>
2020-06-12 09:12:17 +0000

    Fix routing of stocks

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,13 @@ Rails.application.routes.draw do
     resources :tags, only: [:index,:show]
     resources :houses, only: [:index, :show, :create, :edit, :update, :destroy], shallow: true
     resources :rooms, only:[:show, :create, :edit, :update, :destroy], shallow: true
-    resources :stocks, only: [:index, :create, :destroy], shallow: true
   end
   resources :memos, only: [] do
     resources :comments, only: [:create, :destroy], shallow: true
     resources :likes, only: [:create, :destroy], shallow: true
   end
+  resources :stocks, only: [:index, :create, :destroy]
+
 
   devise_for :admin, skip: :all
   devise_scope :admin do
