0fbb740eb15283d107696591d62832435c94f1fb
haruhi.m <halu0502@gmail.com>
2020-06-12 04:43:04 +0000

    Add relationships action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,13 @@
 class RelationshipsController < ApplicationController
-  def index
+  def create
+  	@user = User.find(params[:user_id])
+  	current_user.follow(@user.id)
+		redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+  	@user = User.find(params[:user_id])
+  	current_user.unfollow(@user.id)
+		redirect_back(fallback_location: root_path)
   end
 end
