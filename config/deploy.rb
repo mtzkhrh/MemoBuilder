e8a880b779ceac19e562f6453dd3f610245a7009
haruhi.m <halu0502@gmail.com>
2020-06-14 04:49:10 +0000

    Update config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,12 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "MemoBuilder"
+set :repo_url, "git@github.com:mtzkhrh/MemoBuilder.git"
+set :deploy_to, "/home/ec2-user/MemoBuilder"
+set :rbenv_ruby, '2.5.7'
+set :linked_files, %w{config/master.key .env}
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
