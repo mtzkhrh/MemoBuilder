fa6b2bf285128971c95fe0f63d3e7f54db29f7ee
haruhi.m <halu0502@gmail.com>
2020-06-14 11:49:14 +0000

    Add search friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,20 +12,6 @@ class UsersController < ApplicationController
   	@resent_memos = Memo.includes(:user).where(user_id: @user.id).order(updated_at: :desc).first(6)
   end
 
-  def relationships
-    @user = User.find(params[:id])
-    @q = @user.friends.ransack(params[:q])
-    @friends = @q.result(distinct: true)
-    @followings = @user.followings.where.not(id: @user.friends.pluck(:id))
-    @followers = @user.followers.where.not(id: @user.friends.pluck(:id))
-  end
-
-  def stocks
-    @user = current_user
-    @q = current_user.stock_memos.ransack(params[:q])
-    @stocks = @q.result(distinct: true)
-  end
-
   def edit
   	@user = User.find(params[:id])
   end
@@ -46,6 +32,24 @@ class UsersController < ApplicationController
     flash[:alert]="登録情報を全て削除しました"
     redirect_to root_path
   end
+
+  def stocks
+    @user = current_user
+    @q = current_user.stock_memos.ransack(params[:q])
+    @stocks = @q.result(distinct: true)
+  end
+
+  def relationships
+    @user = User.find(params[:id])
+    @relationships = @user.all_relationships
+    @q = @user.friends.ransack(params[:q])
+    @friends = @q.result(distinct: true)
+    @r = @user.followings.where.not(id: @user.friends.pluck(:id)).ransack(params[:q])
+    @followings = @r.result(distinct: true)
+    @s = @user.followers.where.not(id: @user.friends.pluck(:id)).ransack(params[:q])
+    @followers = @s.result(distinct: true)
+  end
+
 
   private
 
