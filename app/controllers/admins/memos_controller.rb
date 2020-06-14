5d3d4e4edf242951ef79183623e86591a84d6ccb
haruhi.m <halu0502@gmail.com>
2020-06-14 09:29:58 +0000

    Add action to controller for admin

--- app/controllers/admins/memos_controller.rb
+++ app/controllers/admins/memos_controller.rb
@@ -2,11 +2,15 @@ class Admins::MemosController < ApplicationController
 	before_action :authenticate_admin!
 
   def index
+  	@q = Memo.ransack(params[:q])
+  	@memos = @q.result(distinct: true)
   end
 
   def show
+  	@memo = Memo.find(params[:id])
   end
 
   def edit
+  	@memo = Memo.find(params[:id])
   end
 end
--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,11 +2,15 @@ class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
 
   def index
+  	@q = User.ransack(params[:q])
+  	@users = @q.result(distinct: true)
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 end
