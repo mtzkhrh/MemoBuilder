aaf106078f58fabd11637edd14d7a2631dc33d7c
haruhi.m <halu0502@gmail.com>
2020-06-14 10:52:46 +0000

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     sessions:      'users/sessions',
     passwords:     'users/passwords'
   }
+
+  get '/tags/all', to: 'tags#all'
+  get '/memos/all', to: 'memos#all'
+
   resources :users, only:[:index, :show, :edit, :update, :destroy] do
     get :relationships, on: :member
     get :stocks, on: :member
@@ -17,8 +21,6 @@ Rails.application.routes.draw do
     resources :houses, only: [:index, :show, :create, :edit, :update, :destroy], shallow: true
     resources :rooms, only:[:show, :create, :edit, :update, :destroy], shallow: true
   end
-  get '/tags/all', to: 'tags#all'
-  get '/memos/all', to: 'memos#all'
 
   # URLでmemo_idを渡すためにmemosのルートなしでネスト
   resources :memos, only: [] do
@@ -34,9 +36,12 @@ Rails.application.routes.draw do
     post '/admins/sign_in', to: 'admins/sessions#create', as: :admin_session
     delete '/admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
   end
+
   namespace :admins do
     resources :users, only: [:index,:show,:edit,:update,:destroy]
     resources :memos, only: [:index,:show,:edit,:update,:destroy]
   end
+
   get '/admins', to: 'admins/home#top', as: :admin
+
 end
