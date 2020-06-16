5debccc85f58b6866683b26c53bbc1b906a32929
haruhi.m <halu0502@gmail.com>
2020-06-16 13:15:26 +0000

    Add js to selectbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,29 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+	//最初から家が選択されている時のif文
+	let houseVal = $('#memo_house_id').val();
+	if (houseVal !== "") {
+		let selectedTemplate = $(`#rooms-of-house${houseVal}`).html();
+		$('#memo_room_id').remove(); //デフォルトで入っていた子要素のセレクトボックスを削除
+		$('#memo_house_id').after(selectedTemplate);
+	};
+
+	// デフォルトのRoomsセレクトボックスHTML
+	let defaultRoomSelect = $(`.default-rooms`).html();
+
+		//家が変更されてvalueに値が入った時の処理
+	$(document).on('change', '#memo_house_id', function() {
+		let houseVal = $('#memo_house_id').val();
+		if (houseVal !== "") {
+			let selectedTemplate = $(`#rooms-of-house${houseVal}`).html();
+			$('#memo_room_id').remove();
+			$('#memo_house_id').after(selectedTemplate);
+		}else {
+			$('#memo_room_id').remove(); //デフォルトで入っていた子要素のセレクトボックスを削除
+			$('#memo_house_id').after(defaultRoomSelect);
+  	};
+	});
+});
