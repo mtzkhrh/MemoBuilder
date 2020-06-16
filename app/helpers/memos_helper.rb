a8539ff907043f984df544d19bb918b744770ef3
haruhi.m <halu0502@gmail.com>
2020-06-16 10:10:07 +0000

    fix pickup_memos

--- app/helpers/memos_helper.rb
+++ app/helpers/memos_helper.rb
@@ -3,7 +3,7 @@ module MemosHelper
 	# 自分とuserの関係を元にmemosを持ってくる
   def pickup_memos_within_range(parent,user)
   	# userのmemosを得る時
-		if parent.name == "User"
+		if parent.class.name == "User"
 			if user.id == current_user.id #自分の場合
 				@q = user.memos.ransack(params[:q])
 				@memos = @q.result(distinct: true)
@@ -15,7 +15,7 @@ module MemosHelper
 				@memos = @q.result(distinct: true)
 			end
 		# 家のmemosを得る場合
-		elsif parent.name == "House"
+		elsif parent.class.name == "House"
 			if user.id == current_user.id #自分の場合
 				@q = parent.house_memos.ransack(params[:q])
 				@memos = @q.result(distinct: true)
@@ -27,7 +27,7 @@ module MemosHelper
 				@memos = @q.result(distinct: true)
 			end
 		# 部屋のmemosを得る場合
-		elsif parent.name == "Room"
+		elsif parent.class.name == "Room"
 			if user.id == current_user.id #自分の場合
 				@q = parent.memos.ransack(params[:q])
 				@memos = @q.result(distinct: true)
