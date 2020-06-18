15bb18f4e16e5cf91b4a769955f2ae0d1cf522d8
haruhi.m <halu0502@gmail.com>
2020-06-18 13:39:20 +0000

    Move js from app.js into view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,53 +13,8 @@
 //= require rails-ujs
 //= require jquery3
 //= require popper
-//= require summernote/summernote-bs4.min
-//= require summernote-init
 //= require activestorage
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
 
-// メモ投稿画面のjs
-$(document).on('turbolinks:load', function(){
-	// デフォルトのRoomsセレクトボックスHTML
-	let defaultRoomSelect = $(`#rooms-of-houses`).html()
-
-	//画面読み込み時の値でセレクトボックスを切り替える
-	let houseVal = $('#memo_house_id').val();
-	if (houseVal !== "") {
-		let selectedTemplate = $(`#rooms-of-house${houseVal}`).html();
-		$('#memo_house_id').after(selectedTemplate);
-	}else{
-		$('#memo_house_id').after(defaultRoomSelect);
-	};
-	// 最初から部屋が選択されている時の処理
-	let roomVal = $('#memo_room_id').val();
-	if (roomVal !== "") {
-		let houseId = $(`.default-rooms`).find('option:selected').data().houseId
-		$('#memo_house_id').val(houseId)
-	};
-
-	// 部屋を変えた時に家も自動で選択される
-	$(document).on('change', '.default-rooms', function() {
-		let roomVal = $('#memo_room_id').val();
-		if (roomVal !== "") {
-			let houseId = $(`.default-rooms`).find('option:selected').data().houseId
-			$('#memo_house_id').val(houseId)
-		};
-	});
-
-
-		//家が変更されたらその家の部屋が選択肢になる
-	$(document).on('change', '#memo_house_id', function() {
-		let houseVal = $('#memo_house_id').val();
-		if (houseVal !== "") {
-			let selectedTemplate = $(`#rooms-of-house${houseVal}`).html();
-			$('#memo_room_id').remove();
-			$('#memo_house_id').after(selectedTemplate);
-		}else {
-			$('#memo_room_id').remove();
-			$('#memo_house_id').after(defaultRoomSelect);
-  	};
-	});
-});
