e854d65f95e2eac989ae1c004898bbe485fef749
haruhi.m <halu0502@gmail.com>
2020-06-18 10:14:34 +0000

    Resolve N+1 tags/show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,7 +10,7 @@ class TagsController < ApplicationController
   def show
   	@user = User.find(params[:user_id])
   	@tag = Memo.tag_counts.find(params[:id])
-  	@q = Memo.tagged_with(@tag.name).where(user_id: @user.id).ransack(params[:q])
+  	@q = Memo.tagged_with(@tag.name).where(user_id: @user.id).preload(:tags).ransack(params[:q])
     @memos = @q.result(distinct: true)
   end
 end
