47e3f117955fe028afb31123ebdb0ae9f3bd81f8
haruhi.m <halu0502@gmail.com>
2020-06-21 04:48:56 +0000

    Modify N+1 preload & eager_load

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def top
   	@user = User.new
-  	@resent_memos = Memo.all.includes(:user).first(10)
+  	@resent_memos = Memo.all.eager_load(:user).first(10)
   end
 
   def about
