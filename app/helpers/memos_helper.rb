669afcd087ecf44fee92a8c99a95883114e1016e
haruhi.m <halu0502@gmail.com>
2020-06-22 10:28:06 +0000

    Add pagination to all views of data_table_index

--- app/helpers/memos_helper.rb
+++ app/helpers/memos_helper.rb
@@ -6,37 +6,37 @@ module MemosHelper
 		if parent.class.name == "User"
 			if user.id == current_user.id #自分の場合
 				@q = user.memos.resent.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			elsif user.friends.pluck(:id).include?(current_user.id) #友達の場合
 				@q = user.memos.only_friends.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			else #他人の場合
 				@q = user.memos.open.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			end
 		# 家のmemosを得る場合
 		elsif parent.class.name == "House"
 			if user.id == current_user.id #自分の場合
 				@q = parent.house_memos.resent.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			elsif user.friends.pluck(:id).include?(current_user.id) #友達の場合
 				@q = parent.house_memos.only_friends.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			else #他人の場合
 				@q = parent.house_memos.open.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			end
 		# 部屋のmemosを得る場合
 		elsif parent.class.name == "Room"
 			if user.id == current_user.id #自分の場合
 				@q = parent.memos.resent.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			elsif user.friends.pluck(:id).include?(current_user.id) #友達の場合
 				@q = parent.memos.only_friends.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			else #他人の場合
 				@q = parent.memos.open.with_tags.ransack(params[:q])
-				@memos = @q.result(distinct: true)
+				@memos = @q.result(distinct: true).page(params[:page])
 			end
 		end
 	end
