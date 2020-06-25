f11ac3826c032d80256b08d39c6005410b711060
haruhi.m <halu0502@gmail.com>
2020-06-25 07:13:39 +0000

    Add system_test to users

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -25,7 +25,29 @@ describe 'ユーザー認証のテスト' do
         expect(page).to have_content 'エラー'
       end
     end
-    context 'トップページから新規登録をする'
+    context 'トップページに遷移' do
+      before do
+        visit root_path
+      end
+      it '新規登録に成功する' do
+        fill_in 'user[name]', with: Faker::Internet.username(specifier: 5)
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        click_button 'アカウントを作成'
+
+        expect(page).to have_content 'アカウント登録が完了しました。'
+      end
+      it '新規登録に失敗する' do
+        fill_in 'user[name]', with: ''
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        fill_in 'user[password_confirmation]', with: ''
+        click_button 'アカウントを作成'
+
+        expect(page).to have_content 'エラー'
+      end
+    end
   end
   describe 'ユーザーログイン' do
     let(:user) { create(:user) }
@@ -33,10 +55,9 @@ describe 'ユーザー認証のテスト' do
       visit new_user_session_path
     end
     context 'ログイン画面に遷移' do
-      let(:test_user) { user }
       it 'ログインに成功する' do
-        fill_in 'user[email]', with: test_user.email
-        fill_in 'user[password]', with: test_user.password
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
         click_button 'ログイン'
 
         expect(page).to have_content 'ログインしました。'
@@ -48,6 +69,136 @@ describe 'ユーザー認証のテスト' do
         click_button 'ログイン'
 
         expect(current_path).to eq(new_user_session_path)
+      end
+    end
+  end
+end
+describe 'ユーザのテスト' do
+  let!(:user) { create(:user) }
+  let!(:test_user2) { create(:user) }
+  before do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログイン'
+  end
+  describe 'サイドバーのテスト' do
+    context '表示の確認' do
+      it '画像が表示される' do
+        expect(page).to have_css('img.profile_image')
+      end
+      it '名前が表示される' do
+        expect(page).to have_content(user.name)
+      end
+      it '自己紹介が表示される' do
+        expect(page).to have_content(user.introduction)
+      end
+      it '編集リンクが表示される' do
+        visit user_path(user)
+        expect(page).to have_link '', href: edit_user_path(user)
+      end
+    end
+  end
+
+  describe '編集のテスト' do
+    context '自分の編集画面への遷移' do
+      it '遷移できる' do
+        visit edit_user_path(user)
+        expect(current_path).to eq('/users/' + user.id.to_s + '/edit')
+      end
+    end
+    context '他人の編集画面への遷移' do
+      it '遷移できない' do
+        visit edit_user_path(test_user2)
+        expect(current_path).not_to eq('/users/' + user.id.to_s + '/edit')
+      end
+    end
+    context '表示の確認' do
+      before do
+        visit edit_user_path(user)
+      end
+      it '名前編集フォームに自分の名前が表示される' do
+        expect(page).to have_field 'user[name]', with: user.name
+      end
+      it '画像編集フォームが表示される' do
+        expect(page).to have_field 'user[profile_image]'
+      end
+      it '自己紹介編集フォームに自分の自己紹介が表示される' do
+        expect(page).to have_field 'user[introduction]', with: user.introduction
+      end
+      it '編集に成功する' do
+        click_button '更新する'
+        expect(page).to have_content '登録情報を更新しました'
+        expect(current_path).to eq('/users/' + user.id.to_s)
+      end
+      it '編集に失敗する' do
+        fill_in 'user[name]', with: ''
+        click_button '更新する'
+        expect(page).to have_content 'エラー'
+        expect(current_path).to eq('/users/' + user.id.to_s)
+      end
+    end
+  end
+  describe '一覧画面のテスト' do
+    before do
+      visit users_path
+    end
+    context '表示の確認' do
+      it '全ユーザ一覧と表示される' do
+        expect(page).to have_content('全ユーザ一覧')
+      end
+      it '自分と他の人の画像が表示される' do
+        # サイドバーの１枚とリストの２枚
+        expect(all('img').size).to eq(3)
+      end
+      it '自分と他の人の名前が表示される' do
+        expect(page).to have_content user.name
+        expect(page).to have_content test_user2.name
+      end
+      it 'ユーザのリンクが表示される' do
+        expect(page).to have_link user.name, href: user_path(user)
+        expect(page).to have_link test_user2.name, href: user_path(test_user2)
+      end
+      it '検索フォームが表示される' do
+        expect(page).to have_field 'q[name_cont]'
+      end
+    end
+    context '友達ボタンの表示の確認' do
+      it '自分をフォローするリンクは表示されない' do
+        expect(page).not_to eq(have_link '', href: user_relationships_path(user))
+      end
+      it 'お互いフォローしていない時は「友達申請する」' do
+        expect(page).to have_link '友達申請する', href: user_relationships_path(test_user2)
+      end
+      it '自分のみフォローしている時は「友達申請中」' do
+        user.follow(test_user2.id)
+        visit users_path
+        expect(user.followings).to include(test_user2)
+        expect(page).to have_link '友達申請中', href: user_relationships_path(test_user2)
+      end
+      it '相手からのみフォローしている時は「友達申請が来ています」' do
+        test_user2.follow(user.id)
+        visit users_path
+        expect(test_user2.followings).to include(user)
+        expect(page).to have_link '友達申請が来ています', href: user_relationships_path(test_user2)
+      end
+      it 'お互いフォローしていると「友達」' do
+        user.follow(test_user2.id)
+        test_user2.follow(user.id)
+        expect(user.friends?(test_user2)).to eq true
+        expect(page).to have_link '友達', href: user_relationships_path(test_user2)
+      end
+    end
+    context '検索機能のテスト' do
+      it '検索に成功する' do
+        fill_in 'q[name_cont]',with: test_user2.name
+        click_button 'q[submit]'
+        expect(page).to have_content test_user2.name
+      end
+      it '該当なしの時「見つかりませんでした」を表示する' do
+        fill_in 'q[name_cont]',with: Faker::Lorem.characters(number:21)
+        click_button 'q[submit]'
+        expect(page).to have_content '見つかりませんでした'
       end
     end
   end
