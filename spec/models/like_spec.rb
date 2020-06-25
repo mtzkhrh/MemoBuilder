db48eebe927d302dbd9aa28baeebb3bba9446c63
haruhi.m <halu0502@gmail.com>
2020-06-25 02:29:24 +0000

    Add test to like_model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe 'Likeモデルのテスト', type: :model do
+	describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Like.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Memoモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Like.reflect_on_association(:memo).macro).to eq :belongs_to
+      end
+    end
+	end
+end
