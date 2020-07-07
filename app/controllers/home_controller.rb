ce839606bf5a415a012265b30e94b1443ea54365
haruhi.m <halu0502@gmail.com>
2020-07-07 09:08:04 +0000

    Resolve N+1 in home/top

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def top
     @user = User.new
-    @memos = Memo.open.resent.first(20)
+    @memos = Memo.open.with_user.resent.first(20)
   end
 
   def about
