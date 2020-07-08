30cf4628dd26f6f45af3fb1b82a483ed7f3830e5
haruhi.m <halu0502@gmail.com>
2020-07-08 06:09:23 +0000

    Modify by rubocop running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,35 @@
 class ApplicationController < ActionController::Base
   protected
-	# 自分とuserの関係を元にmemosを持ってくる
+
+  # 自分とuserの関係を元にmemosを持ってくる
   def pickup_memos_within_range(parent, user)
     # userのmemosを得る時
     if parent.class.name == "User"
-    	pickup_memos_from_user(user)
+      pickup_memos_from_user(user)
 
     # 家のmemosを得る場合
     elsif parent.class.name == "House"
-    	pickup_memos_from_house(parent, user)
+      pickup_memos_from_house(parent, user)
 
     # 部屋のmemosを得る場合
     elsif parent.class.name == "Room"
-    	pickup_memos_from_room(parent, user)
+      pickup_memos_from_room(parent, user)
     end
   end
 
   def pickup_memos_from_user(user)
-    if user.id == current_user.id #自分の場合
+    if user.id == current_user.id # 自分の場合
       @q = user.memos.resent.with_tags.ransack(params[:q])
       @memos = @q.result(distinct: true).page(params[:page])
-    elsif user.friends.pluck(:id).include?(current_user.id) #友達の場合
+    elsif user.friends.pluck(:id).include?(current_user.id) # 友達の場合
       @q = user.memos.only_friends.with_tags.ransack(params[:q])
       @memos = @q.result(distinct: true).page(params[:page])
-    else #他人の場合
+    else # 他人の場合
       @q = user.memos.open.with_tags.ransack(params[:q])
       @memos = @q.result(distinct: true).page(params[:page])
     end
   end
+
   def pickup_memos_from_house(house, user)
     if user.id == current_user.id
       @q = house.house_memos.resent.with_tags.ransack(params[:q])
@@ -40,6 +42,7 @@ class ApplicationController < ActionController::Base
       @memos = @q.result(distinct: true).page(params[:page])
     end
   end
+
   def pickup_memos_from_room(room, user)
     if user.id == current_user.id
       @q = room.memos.resent.with_tags.ransack(params[:q])
