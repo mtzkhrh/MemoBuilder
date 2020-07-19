10a8c6d13fb05bc6fbb3c5b5aff75f78aade2553
haruhi.m <halu0502@gmail.com>
2020-07-19 06:59:38 +0000

    Add display test to rooms/index

--- spec/system/room_spec.rb
+++ spec/system/room_spec.rb
@@ -31,6 +31,36 @@ RSpec.describe 'Room', type: :system do
           expect(current_path).to eq('/users/' + test_user2.id.to_s + '/rooms')
       	end
       end
+
+      context '表示の確認' do
+				it '部屋の作成フォームが表示される' do
+          expect(page).to have_field 'room[name]'
+        end
+        it '他人の一覧には部屋作成フォームが表示されない' do
+        	visit user_rooms_path(test_user2)
+          expect(page).not_to have_field 'room[name]'
+          expect(current_path).to eq('/users/' + test_user2.id.to_s + '/rooms')
+        end
+        it '検索フォームが表示される' do
+          expect(page).to have_field 'q[name_cont]'
+        end
+        it '並び替えリンクが表示される' do
+        	expect(page).to have_link '名前順'
+        	expect(page).to have_link '更新順'
+        end
+        it '「部屋一覧」が表示される' do
+        	expect(page).to have_content '部屋一覧'
+        end
+        it '部屋の名前のリンクが表示される' do
+        	expect(page).to have_link room1.name, href: room_path(room1)
+        	expect(page).to have_link room2.name, href: room_path(room2)
+        	expect(page).to have_link room3.name, href: room_path(room3)
+        end
+        it 'マイページへのリンクが表示される' do
+          expect(page).to have_link '<< マイページへ', href: user_path(user)
+        end
+
+      end
     end
   end
 end
